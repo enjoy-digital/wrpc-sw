f6fcc6dea7241318dff06cc8de73cd3bab022927
Tristan Gingold <tristan.gingold@cern.ch>
2026-01-09 10:48:35 +0100

    softpll.mk: factorize

--- softpll/softpll.mk
+++ softpll/softpll.mk
@@ -1,4 +1,4 @@
-obj-$(CONFIG_ARCH_LM32) += \
+obj-softpll = \
 	softpll/spll_common.o \
 	softpll/spll_external.o \
 	softpll/spll_helper.o \
@@ -6,10 +6,7 @@ obj-$(CONFIG_ARCH_LM32) += \
 	softpll/spll_ptracker.o \
 	softpll/softpll_ng.o
 
-obj-$(CONFIG_ARCH_RISCV) += \
-	softpll/spll_common.o \
-	softpll/spll_external.o \
-	softpll/spll_helper.o \
-	softpll/spll_main.o \
-	softpll/spll_ptracker.o \
-	softpll/softpll_ng.o
+# softpll is used for nodes and for switch
+# But not for host process.
+obj-$(CONFIG_EMBEDDED_NODE) += $(obj-softpll)
+obj-$(CONFIG_TARGET_WR_SWITCH) += $(obj-softpll)
